1ab7a6d46f7b30be435da951441931bd6201f992
poolbooyer <ryou3018@gmail.com>
2019-11-20 23:34:11 +0900

    [fix]ブラウザタブ操作の適用

--- get.js
+++ get.js
@@ -1,14 +1,14 @@
 function getCall(event){// XMLHttpRequestオブジェクトの作成
-  var request = new XMLHttpRequest()
-  let call=document.forms.query.callsign.value
+  let request = new XMLHttpRequest()
+  const call=document.forms.query.callsign.value
   // URLを開く
   request.open('GET', `https://www.tele.soumu.go.jp/musen/list?ST=1&DA=0&DC=1&SC=1&OF=2&OW=AT&MA=${call}`, true)
   request.responseType = 'json'
   // レスポンスが返ってきた時の処理を記述 
   request.onload = function () {
     data=this.response
-    console.log(data)
-    let output=''
+
+    let output =`<p>入力:<span id="inputValue">${call}</span></p>`
     // レスポンスが返ってきた時の処理
     if(data['musen'].length>5){
       for (let i = 0, l = 5; i < l; i++) {
@@ -18,7 +18,7 @@ function getCall(event){// XMLHttpRequestオブジェクトの作成
           <dt>所在地</dt><dd>${data['musen'][i]['listInfo']['tdfkCd']}</dd>
           </dl>`
       }
-      output=output+`<a href="https://www.tele.soumu.go.jp/musen/SearchServlet?SC=1&pageID=3&SelectID=1&CONFIRM=0&SelectOW=01&IT=&HC=&HV=&FF=&TF=&HZ=3&NA=&MA=${call}&DFY=&DFM=&DFD=&DTY=&DTM=&DTD=&SK=2&DC=100#result">他の結果はこちら</a>`
+      output=output+`<p id="viewMore">他の結果はこちら</p>`
     }else {
       for (let i=0,l = data['musen'].length; i < l;i++){
         output=output+`<h2>結果 ${i+1}</h2>`
@@ -29,10 +29,16 @@ function getCall(event){// XMLHttpRequestオブジェクトの作成
       }
     }
     document.getElementById('result').innerHTML = output
+    document.getElementById('viewMore').addEventListener('click', openView)
   }
-
   // リクエストをURLに送信
   request.send()
 }
-
+function openView(event){
+  const call = document.getElementById('inputValue').textContent
+  const target = `https://www.tele.soumu.go.jp/musen/SearchServlet?SC=1&pageID=3&SelectID=1&CONFIRM=0&SelectOW=01&IT=&HC=&HV=&FF=&TF=&HZ=3&NA=&MA=${call}&DFY=&DFM=&DFD=&DTY=&DTM=&DTD=&SK=2&DC=100#result`
+  chrome.tabs.create(
+    { url :  target }
+  )
+}
 document.getElementById('check').addEventListener('click',getCall)
